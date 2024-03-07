d48472f6e54e102b41cc15723039dbb3914e96db
Nikolai Degtiarev <NikAndDeg@yandex.ru>
2024-03-07 15:16:41 +0500

    checkstyle 2

--- src/test/java/ru/yandex/practicum/filmorate/repository/film/FilmDbRepositoryImplTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/repository/film/FilmDbRepositoryImplTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
